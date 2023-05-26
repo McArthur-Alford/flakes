ea6668e264c14ba2156975ff04db9de577d369ce
mcarthur <mcarthur.alford@proton.me>
2023-05-26 10:54:36 +1000

    Added template thingy

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A collection of templates";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+    (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        templates."rust" = {
+          path = ./rust;
+          description = "rust";
+        };
+      }
+    );
+
+  # templates = {
+  #   rust = {
+  #     path = ./rust;
+  #     description = "Rust";
+  #   };
+  # };
+}
