cde860cf6ab33bf4554988186385da18184c976a
mcarthur <mcarthur.alford@proton.me>
2023-05-27 22:21:14 +1000

    Removed comments

--- flake.nix
+++ flake.nix
@@ -1,21 +1,6 @@
 {
   description = "A collection of templates";
 
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
-  # inputs.flake-utils.url = "github:numtide/flake-utils";
-
-
-  # outputs = { self, nixpkgs, flake-utils }:
-  #   flake-utils.lib.eachDefaultSystem
-  #   (system:
-  #     let pkgs = nixpkgs.legacyPackages.${system}; in {
-  #       templates.rust = {
-  #         path = ./rust;
-  #         description = "rust";
-  #       };
-  #     }
-  #   );
-
   outputs = { self }: {
     templates = {
       rust = {
