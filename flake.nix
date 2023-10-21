16cbf7465d0f25edf2140a373061bfeda2ec9b97
mcarthur <mcarthur.alford@proton.me>
2023-10-21 13:30:08 +1000

    Cleanup started

--- flake.nix
+++ flake.nix
@@ -6,6 +6,18 @@
       rust = {
         path = ./rust;
         description = "Rust";
+        welcomeText = ''
+          # A simple Rust/Cargo Template
+          Provides access to rust and cargo. Obviously.
+        '';
+      };
+      default = {
+        path = ./default;
+        description = "Default";
+        welcomeText = ''
+          # A empty, default template
+          ...to modify to your hearts content
+        '';
       };
     };
   };
