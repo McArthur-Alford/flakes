29214beab55bc8688e286014377422c5886c50f4
mcarthur <mcarthur.alford@proton.me>
2023-10-21 20:50:06 +1000

    Updated languages

--- flake.nix
+++ flake.nix
@@ -27,22 +27,34 @@
         path = ./default;
         description = "Default";
         welcomeText = ''
-          # A empty, default template
-          ...to modify to your hearts content
+        # A empty, default template
+        ...to modify to your hearts content
         '';
       };
       rust = {
         path = ./rust;
         description = "Rust";
         welcomeText = ''
-          # A simple Rust/Cargo Template
-          Provides access to rust and cargo. Obviously.
+        # A simple Rust/Cargo template
+        Provides access to rust and cargo. Obviously.
         '';
       };
       # haskell = {};
       # java = {};
-      # python = {};
-      # pytorch = {};
+      python311 = {
+        path = ./python311;
+        description = "Python311";
+        welcomeText = ''
+        # A simple python 3.11 template
+        '';
+      };
+      torchrocm = {
+        path = ./torchrocm;
+        description = "Python311 With Pytorch/ROCM";
+        welcomeText = ''
+        # A convoluted python 3.11 template with torch/ROCM support
+        '';
+      };
       # c = {};
     };
   };
